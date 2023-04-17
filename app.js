524030d9c9f5ccb1e25b3b34ee3250050a28ffaf
AlekberliS <117600954+AlekberliS@users.noreply.github.com>
2023-04-17 12:05:24 +0400

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,16 @@
 var itemInput = document.getElementById("itemInput");
 var itemList = document.getElementById("groceryItems");
 
-let items;
-if (localStorage.getItem('items')) {
-  items = JSON.parse(localStorage.getItem('items'));
-} else {
-  items = [];
-}
 
 
 // add items 
 function addItem() {
 	// Get the item value from the input field
 	var itemValue = itemInput.value.trim();
-
+    
+    // const inputValue = itemInput.value;
+    localStorage.setItem("gitems", JSON.stringify(itemValue));
+    console.log(localStorage);
 	// Check if the item value is not empty
 	if (itemValue !== "") {
 		// Create a new list item
@@ -27,7 +24,7 @@ function addItem() {
 		doneBtn.innerHTML = '<i class="fas fa-edit fa-xs" style="color: #13d110; background-color: white; float:right; margin-left:5px"></i>';
 		doneBtn.addEventListener("click", function () {
 			li.classList.add("done-item");
-	
+
 
 		});
 
@@ -36,9 +33,10 @@ function addItem() {
 		clearBtn.innerHTML = '<i class="fas fa-trash fa-xs" style="float: right; color:red; margin-left:5px;"> </i>';
 		clearBtn.addEventListener("click", function () {
 			itemList.removeChild(li);
+            localStorage.removeItem("gitems");
 		});
 		// Add the buttons to the list item
-		
+
 		li.appendChild(clearBtn);
 		li.appendChild(doneBtn);
 
@@ -54,8 +52,10 @@ function addItem() {
 }
 
 
+
+
 // Clear all items from the list
 function clearItems() {
 	itemList.innerHTML = "";
-	// localStorage.clear();
+	 localStorage.clear();
 }
